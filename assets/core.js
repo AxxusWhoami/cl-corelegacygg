7f5b791606c4da14a8f7e1bd847c6377f69c6c94
Javi Martinez <info@axxusentertainment.com>
2026-09-19 20:31:02 +0200

    Add Meta Pixel to core.js

--- assets/core.js
+++ assets/core.js
@@ -99,4 +99,28 @@
       window.scrollTo({ top: 0, behavior: 'smooth' });
     });
   })();
+
+  // ===== Meta Pixel (deferred for performance) =====
+  (function metaPixel() {
+    function load() {
+      if (window.fbq) return;
+      var n = window.fbq = function () {
+        n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
+      };
+      if (!window._fbq) window._fbq = n;
+      n.push = n; n.loaded = true; n.version = '2.0'; n.queue = [];
+      var t = document.createElement('script');
+      t.async = true;
+      t.src = 'https://connect.facebook.net/en_US/fbevents.js';
+      var s = document.getElementsByTagName('script')[0];
+      s.parentNode.insertBefore(t, s);
+      fbq('init', '1311164035421702');
+      fbq('track', 'PageView');
+    }
+    if (document.readyState === 'complete') {
+      load();
+    } else {
+      window.addEventListener('load', load);
+    }
+  })();
 })();
